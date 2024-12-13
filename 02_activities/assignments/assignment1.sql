3d5d3779a4b595083d1b78eb24396d282a14b75d
jcpeinado <julianpeinado.996@gmail.com>
2024-12-13 18:07:41 -0500

    Section_2 update 12-13-2024

--- 02_activities/assignments/assignment1.sql
+++ 02_activities/assignments/assignment1.sql
@@ -5,21 +5,33 @@
 --SELECT
 /* 1. Write a query that returns everything in the customer table. */
 
+SELECT *
+FROM customer
+;
 
 
 /* 2. Write a query that displays all of the columns and 10 rows from the cus- tomer table, 
 sorted by customer_last_name, then customer_first_ name. */
 
+SELECT *
+FROM customer
+ORDER BY customer_last_name, customer_first_name
+LIMIT 10
+;
 
 
 --WHERE
 /* 1. Write a query that returns all customer purchases of product IDs 4 and 9. */
 -- option 1
 
+SELECT *
+FROM customer_purchases
+WHERE customer_id = 4
+OR customer_id = 9
+;
+
 
 -- option 2
-
-
 
 /*2. Write a query that returns all customer purchases and a new calculated column 'price' (quantity * cost_to_customer_per_qty), 
 filtered by vendor IDs between 8 and 10 (inclusive) using either:
@@ -27,9 +39,17 @@ filtered by vendor IDs between 8 and 10 (inclusive) using either:
 	2.  one condition using BETWEEN
 */
 -- option 1
-
+SELECT *, (quantity* cost_to_customer_per_qty) as price
+FROM customer_purchases
+WHERE vendor_id <= 10
+AND vendor_id >= 8
+;
 
 -- option 2
+SELECT *, (quantity* cost_to_customer_per_qty) as price
+FROM customer_purchases
+WHERE vendor_id BETWEEN 8 AND 10
+;
 
 
 
@@ -39,27 +59,57 @@ Using the product table, write a query that outputs the product_id and product_n
 columns and add a column called prod_qty_type_condensed that displays the word “unit” 
 if the product_qty_type is “unit,” and otherwise displays the word “bulk.” */
 
+SELECT product_id, product_name
+	,CASE WHEN product_qty_type = 'unit'
+		THEN 'unit'
+		ELSE 'bulk'
+	END as prod_qty_type
 
+FROM product
+;
+		
 
 /* 2. We want to flag all of the different types of pepper products that are sold at the market. 
 add a column to the previous query called pepper_flag that outputs a 1 if the product_name 
 contains the word “pepper” (regardless of capitalization), and otherwise outputs 0. */
+SELECT product_id, product_name
+	,CASE WHEN product_qty_type = 'unit'
+		THEN 'unit'
+		ELSE 'bulk'
+	END as prod_qty_type
 
+	,CASE WHEN product_name like '%eppers%' or '%PEPPERS%'
+		THEN 1
+		ELSE 0
+	END as pepper_flag
+	
+FROM product
+;
 
 
 --JOIN
 /* 1. Write a query that INNER JOINs the vendor table to the vendor_booth_assignments table on the 
 vendor_id field they both have in common, and sorts the result by vendor_name, then market_date. */
 
-
-
-
+SELECT vendor_name, 
+vendor_booth_assignments.market_date, 
+vendor.vendor_id,
+vendor_booth_assignments.vendor_id
+FROM vendor
+INNER JOIN vendor_booth_assignments
+	ON vendor.vendor_id = vendor_booth_assignments.vendor_id
+ORDER by vendor_name, market_date DESC
+;
 /* SECTION 3 */
 
 -- AGGREGATE
 /* 1. Write a query that determines how many times each vendor has rented a booth 
 at the farmer’s market by counting the vendor booth assignments per vendor_id. */
+SELECT vendor_id, count(vendor_id) as num_of_times_rented
+FROM vendor_booth_assignments
+GROUP by vendor_id
 
+;
 
 
 /* 2. The Farmer’s Market Customer Appreciation Committee wants to give a bumper 
